32f6104994789a5fc61c3f6ff1dc7208514fb05b
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2018-10-23 13:56:49 -0400

    heroku psql connection string

--- queries.js
+++ queries.js
@@ -9,7 +9,8 @@ var pgp = require('pg-promise')(options);
 
 var connectionString;
 if (process.env.NODE_ENV === "production") {
-  connectionString = 'postgres://ipdbyoxmmnzdyn:0d63d49d3dea6c50cf524e1f7e0153f20ea30108c024c48263d5f3d751b243a5@ec2-107-20-211-10.compute-1.amazonaws.com:5432/ddrpd50b0fhsgn';
+  const { DATABASE_URL } = process.env
+  connectionString = DATABASE_URL;
 } else {
   connectionString = 'postgres://localhost:5432/mediar';
 }
