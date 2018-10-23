b6894051a145a145bcfb9c68f775391314f3959f
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2018-10-23 13:18:45 -0400

    more environment variable tinkering

--- queries.js
+++ queries.js
@@ -9,7 +9,7 @@ var pgp = require('pg-promise')(options);
 
 var connectionString;
 if (process.env.NODE_ENV === "prod") {
-  connectionString = process.env.DATABASE_URL;
+  connectionString = 'postgres://ipdbyoxmmnzdyn:0d63d49d3dea6c50cf524e1f7e0153f20ea30108c024c48263d5f3d751b243a5@ec2-107-20-211-10.compute-1.amazonaws.com:5432/ddrpd50b0fhsgn';
 } else {
   connectionString = 'postgres://localhost:5432/mediar';
 }
